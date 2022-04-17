316688cf86068d286d9a1bd6cb6370ef1bd69780
Muskan Bagrecha <muskanbagrecha04@gmail.com>
2022-04-18 02:14:13 +0530

    minor fixes

--- src/components/UI/navigation/Navigation.jsx
+++ src/components/UI/navigation/Navigation.jsx
@@ -1,12 +1,14 @@
-import { NavLink } from "react-router-dom";
+import { useNavigate, NavLink } from "react-router-dom";
 import "./Navigation.css";
 import logo from "../../../assets/logo.png";
 export const Navigation = () => {
+  const navigate = useNavigate();
+
   return (
     <nav className="navigation">
-      <a href="index.html">
+      <NavLink to="/">
         <img src={logo} alt="logo" className="logo" />
-      </a>
+      </NavLink>
       <form className="searchbar">
         <input type="text" placeholder="Search" />
         <button className="btn-icon" type="submit">
@@ -16,9 +18,12 @@ export const Navigation = () => {
 
       <ul className="no-list-style nav__list--static">
         <li>
-          <NavLink to="/">
-            <button className="btn btn-transparent-black-br">Login</button>
-          </NavLink>
+          <button
+            className="btn btn-transparent-black-br"
+            onClick={() => navigate("/")}
+          >
+            Login
+          </button>
         </li>
         <li>
           <button className="theme-toggle btn btn-transparent-black-br">
