bffc52c19a656dec84aed4ef8bd9902cb5df8b7c
Muskan Bagrecha <muskanbagrecha04@gmail.com>
2022-05-24 17:41:00 +0530

    fix: minor fix

--- src/backend/db/quizzes.js
+++ src/backend/db/quizzes.js
@@ -111,13 +111,6 @@ export const quizzes = [
         options: ["Rachael", "Ross", "Joey", "Chandler"],
         answer: 3,
       },
-      {
-        _id: uuid(),
-        question:
-          "Which of the following was not the name of one of Alice and Frank Jr.'s kids?",
-        options: ["Leslie", "Ursula", "Chandler", "Frank Jr. Jr."],
-        answer: 2,
-      },
     ],
     categoryName: "TV Shows",
   },
@@ -130,24 +123,47 @@ export const quizzes = [
     mcqs: [
       {
         _id: uuid(),
-        question: "Which season Harvey is the Most Badass One",
-        options: ["Season 1", "Season 2", "Season 6", "Every Season"],
+        question:
+          "Why did Johnny originally purchase the town of Schitt's Creek?",
+        options: [
+          "For Alexis",
+          "For David",
+          "As a gift for Moira",
+          "For Rose Video store",
+        ],
+        answer: 2,
+      },
+      {
+        _id: uuid(),
+        question: "What is the name of the town's mayor?",
+        options: ["Mike", "Roland", "Johnny", "Mutt"],
         answer: 4,
       },
       {
         _id: uuid(),
-        question: "In which season does Mike was imprisoned",
-        options: ["Season 3", "Season 5", "Season 7", "Season 4"],
+        question: "What does the sign that Roland adds to the town sign say?",
+        options: [
+          "It's not polite to stare!",
+          "We're family!",
+          "Schitt welcomes you home",
+          "Don't worry, it's his sister.",
+        ],
         answer: 4,
       },
       {
         _id: uuid(),
-        question: "Harvey had what kind of emotional problems",
-        options: ["Attachment Issues", "Panic Attack", "Trauma", "Stress"],
-        answer: 1,
+        question:
+          "What recipe are Moira and David trying to make when they fold in the cheese?",
+        options: ["Mac and Cheese", "Quesadillas", "Lasagna", "Enchiladas"],
+        answer: 4,
+      },
+      {
+        _id: uuid(),
+        question: "What gift does Mutt surprise Alexis with?",
+        options: ["A pair of shoes", "A bike", "A pair of glasses", "A dog"],
       },
     ],
-    categoryName: "TV Shows",
+    categoryName: 2,
   },
   {
     _id: uuid(),
@@ -157,23 +173,96 @@ export const quizzes = [
     mcqs: [
       {
         _id: uuid(),
-        question: "Which season Harvey is the Most Badass One",
-        options: ["Season 1", "Season 2", "Season 6", "Every Season"],
-        answer: "Every Season",
+        question:
+          "What command is used to start the React local development server?",
+        options: ["npm run dev", "npm build", "npm serve", "npm start"],
+        answer: 4,
       },
       {
         _id: uuid(),
-        question: "In which season does Mike was imprisoned",
-        options: ["Season 3", "Season 5", "Season 7", "Season 4"],
-        answer: "Season 4",
+        question: "What is the children prop?",
+        options: [
+          "A property that lets you pass data.",
+          "A property to nest components.",
+          "A property to set an object as property",
+          "A property to add child values to state",
+        ],
+        answer: 2,
       },
       {
         _id: uuid(),
-        question: "Harvey had what kind of emotional problems",
-        options: ["Attachment Issues", "Panic Attack", "Trauma", "Stress"],
-        answer: "Attachment Issues",
+        question:
+          "A copy of the 'real' DOM that is kept in memory is called what?",
+        options: ["Shadow DOM", "DOM", "Virtual DOM", "React DOM"],
+        answer: 3,
+      },
+      {
+        _id: uuid(),
+        question:
+          "Which operator can be used to conditionally render a React component?",
+        options: ["&&", "||", "?", "!"],
+        answer: 1,
+      },
+      {
+        _id: uuid(),
+        question:
+          "When rendering a list using the JavaScript map() method, what is required for each element rendered?",
+        options: ["key", "index", "data", "id"],
+        answer: 1,
       },
     ],
     categoryName: "Technology",
+  },
+  {
+    _id: uuid(),
+    title: "Chess",
+    totalScore: 20,
+    thumbnail:
+      "https://images.unsplash.com/photo-1580541832626-2a7131ee809f?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1177",
+    mcqs: [
+      {
+        _id: uuid(),
+        question: "How many squares are there on a chess board?",
+        options: ["32", "64", "99", "65"],
+        answer: 2,
+      },
+      {
+        _id: uuid(),
+        question: "Who were the opponents in the famous Evergreen Game?",
+        options: [
+          "Adolf Anderssen vs. Jean Dufresne",
+          "Paul Morphy vs. Howard Staunton",
+          "Wilhelm Steinitz vs. Emanuel Lasker",
+          "Jose Raul Capablanca vs. Alexander Alekhine",
+        ],
+        answer: 1,
+      },
+      {
+        _id: uuid(),
+        question: "Who was the first official World Chess Champion?",
+        options: [
+          "Garry Kasparov",
+          "Bobby Fischer",
+          "Wilhelm Steinitz",
+          "Paul Morphy",
+        ],
+        answer: 3,
+      },
+      {
+        _id: uuid(),
+        question:
+          "Other than world champion, what is the highest title a chess player can earn?",
+        options: ["Master", "Grandmaster", "Expert", "Challenger"],
+        answer: 2,
+      },
+      {
+        _id: uuid(),
+        question:
+          "What is the name of the chess piece that can move diagonally?",
+        options: ["Bishop", "Rook", "Queen", "Knight"],
+        answer: 1,
+      },
+    ],
+    categoryName: "Sports",
   },
 ];
